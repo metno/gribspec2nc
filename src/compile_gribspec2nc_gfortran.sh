a82f437dead5a6dd48696f78b15aac769bc7d56c
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-04-27 14:18:19 +0200

    Added build script for gfortran

---
+++ src/compile_gribspec2nc_gfortran.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export GRIB_API_INCLUDE="-I/usr/local/include"
+export GRIB_API_LIB="-L/usr/local/lib -lgrib_api_f90 -lgrib_api"
+
+export NETCDF_INCLUDE="$(nf-config --fflags)"
+export NETCDF_LIB="$(nf-config --flibs) -lnetcdff"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+echo $LD_LIBRARY_PATH
+
+rm -fv netcdf_metno_spec.mod
+rm -fv netcdf_metno_spec.o
+
+gfortran $NETCDF_INCLUDE $NETCDF_LIB -lnetcdff -c netcdf_metno_spec.f90
+gfortran -g -O0 gribspec2nc.f90 sphere.f netcdf_metno_spec.f90 $NETCDF_INCLUDE $NETCDF_LIB $GRIB_API_INCLUDE $GRIB_API_LIB -o gribspec2nc
